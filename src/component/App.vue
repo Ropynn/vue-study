8bd56e06188a78aa8d35d9632fb37be11389d95d
Ropynn <ropynn@qq.com>
2017-11-18 11:19:50 +0800

    '集成mint-ui,封装header公共文件'

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,23 +1,11 @@
 <template>
-  <main>
-    <p>{{msg}}</p>
-    <router-view></router-view>
-  </main>
+  <app-header></app-header>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      msg: "测试vue文件是否可用"
-    };
-  }
-};
+
 </script>
 
 <style>
-p {
-  font-size: 30px;
-  color: red;
-}
+
 </style>
---
+++ src/component/common/index.js
@@ -0,0 +1,11 @@
+//编写属于自己的公共vue组件库
+import HeaderComponent from './header.vue';
+
+//vue插件要求提供一个install方法， 这个方法会被注入vue
+//需要我们调用Vue的filter component directive去扩展功能
+export default {
+  install (Vue) {
+    Vue.component('app-header', HeaderComponent);
+  }
+};
+
